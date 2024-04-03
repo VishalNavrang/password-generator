d654229693e11204e55be75244d54970d65c5ed3
Vishal Navrang <162941370+VishalNavrang@users.noreply.github.com>
2024-04-03 12:56:16 +0530

    Add files via upload

---
+++ vishal3.js
@@ -0,0 +1,12 @@
+function generatePassword() {
+    const length = 12; // Change the length of the password here
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    document.getElementById("password").value = password;
+}
+
+document.getElementById("generateBtn").addEventListener("click", generatePassword);
